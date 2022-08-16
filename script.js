eeceacaa68328b75bbd9eea068515edab3add00a
Bryce Tank <brycetank10@gmail.com>
2022-08-16 00:07:12 -0400

    Add buttons that transition, Create round counter -Created round counter for multiple games -The 3 main buttons now transition when hovered and clicked -Restart button also can transition

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 let compChoice;
 let playerInput;
 let outcomeText="";
-let secondaryOutcome="";
 let compScore=0;
 let playerScore=0;
+let roundCounter=1;
+let playerTotalWins=0;
+let compTotalWins=0;
 
 const score=document.querySelector('.score')
 const decisionOutcome=document.querySelector('.decisionOutcome')
-
+const choices=document.querySelector('.choices')
+const resultElementCreation=document.querySelector('.results')
+decisionOutcome.style.cssText='color:rgb(46, 255, 175)'
 
 function createRestart(){
-    const restartButtonCreation=document.querySelector('.results')
+    const roundWinCounter=document.createElement('p')
+    resultElementCreation.appendChild(roundWinCounter)
+    roundWinCounter.classList.add('roundWinCounter')
+    roundWinCounter.innerHTML=`Total Round Wins<br/><br/>You: ${playerTotalWins} ${'\xa0'.repeat(20)} Computer: ${compTotalWins}`
     const restartButton=document.createElement('button')
     restartButton.textContent="Go Again?"
-    restartButton.classList.add('.restartButton')
-    restartButtonCreation.appendChild(restartButton)
+    restartButton.classList.add('restartButton')
+    resultElementCreation.appendChild(restartButton)
     restartButton.addEventListener('click', function restart(){
-        decisionOutcome.textContent="Let's Begin";
+        roundCounter+=1
+        decisionOutcome.textContent=`Commence Round ${roundCounter}`;
+        choices.textContent=''
         score.textContent=''
         compScore=0;
         playerScore=0;
-        restartButtonCreation.removeChild(restartButton)
-
+        score.style.cssText='border:none'
+        resultElementCreation.removeChild(roundWinCounter)
+        resultElementCreation.removeChild(restartButton)
+        decisionOutcome.style.cssText='color:rgb(46, 255, 175)'
 })}
 
 function getComputerChoice(){
@@ -38,54 +49,66 @@ function getComputerChoice(){
     return compChoice;
 }
 
-function decideWinner(){
+function displayChoices(playerInput,compChoice){
+    choices.textContent=
+    `You Chose: ${playerInput.charAt(0).toUpperCase()+playerInput.slice(1)}  ${'\xa0'.repeat(30)} Computer Chose: ${compChoice.charAt(0).toUpperCase()+compChoice.slice(1)}`
+}
+
+function decideWinnerWithMessages(){
     getComputerChoice()
-    console.log("Computer: "+compChoice)
-    console.log("You: "+playerInput)
+    displayChoices(playerInput,compChoice)
     if (playerInput==compChoice){
-        outcomeText="It's a tie, try again"
+        outcomeText="Tie, try again"
+        decisionOutcome.style.cssText='color:white'
     }
     else if (playerInput=='scissors' && compChoice=='paper'||
             playerInput=='rock' && compChoice=='scissors'||
             playerInput=='paper' && compChoice=='rock'){
+        decisionOutcome.style.cssText='color:yellow'
         if (playerScore==4){
             playerScore+=1
-            outcomeText='WINNNNNNNNNN'
+            playerTotalWins+=1
             createRestart()
+            if(compScore==0){
+                outcomeText="Astounding Victory!!!<br/>Poor soul never stood a chance<br/>You Just Used Up The Rest Of Your Luck For The Week"
+            }
+            else if(compScore<=2){
+                outcomeText="Impressive Win!<br/>You did them dirty"
+            }
+            else if (compScore<4){
+                outcomeText="Congrats On The Dub<br/>Don't let them catch you slacking"
+            }
+            else{
+                outcomeText="Hard-fought Victory, PHEW!<br/>That was too close for comfort"
+            }
         }
         else{
-        playerScore+=1
-        outcomeText='You won!'
+            playerScore+=1
+            outcomeText='You won the round!'
     }}
     else {
+        decisionOutcome.style.cssText='color:bright-red'
         if(compScore==4){
             compScore+=1
+            compTotalWins+=1
             createRestart()
-            outcomeText='Heartbreaking Defeat...\nThe Choke Of A Century'
-            // if(playerScore==0){
-            //     outcomeText=`Horrifying Defeat\nDid You Even Try?`
-
-            // }
-            // else if(playerScore<=2){
-            //     outcomeText=`Significant Margin Loss\nGet Good Mate`
-
-            // }
-            // else if (playerScore<4){
-            //     outcomeText='Tough Loss. Maybe Next Time Champ'
-
-            // }
-            // else{
-            //     outcomeText1='Heartbreaking Defeat...'
-            //     outcomeText2='The Choke Of A Century'
-            // }
+            if(playerScore==0){
+                outcomeText="Horrifying Defeat...<br/>Did you even try?"
+            }
+            else if(playerScore<=2){
+                outcomeText="Loss By Significant Margin<br/>Get good mate"
+            }
+            else if (playerScore<4){
+                outcomeText="Tough Loss<br/>Maybe next time champ"
+            }
+            else{
+                outcomeText="Heartbreaking Defeat... <br/>The choke of a century"
+            }
         }
         else{
-        compScore+=1
-        outcomeText='You lost...'
-    }}
-}
-
-
+            compScore+=1
+                outcomeText='Lost round...'
+    }}}
 
     const playerButtons=document.querySelectorAll('.button');
     playerButtons.forEach((button) => {
@@ -95,8 +118,8 @@ function decideWinner(){
             }
             else{
             playerInput=button.id
-            decideWinner()
-            decisionOutcome.textContent=outcomeText+secondaryOutcome
+            decideWinnerWithMessages()
+            decisionOutcome.innerHTML=outcomeText
+            score.style.cssText='border:solid white;'
             score.textContent=`Your Score: ${playerScore} | Computer Score: ${compScore}`
     }})});
-
